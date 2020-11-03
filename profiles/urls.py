c6db4be020344259726457e89c46714ffab083ca
David Duyile <femiduyile@gmail.com>
2020-11-03 18:45:39 +0100

    comp

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,8 +5,8 @@ app_name = 'profiles'
 
 urlpatterns = [
     path('<pk>/', views.ProfileView.as_view(), name='profile'),
+    path('', views.ProfileList.as_view(), name='profile-list'),
     path('update_profile/<pk>', views.updateprofile, name='update_profile'),
     path('follow/<pk>', views.follow, name='follow'),
-
 
 ]
